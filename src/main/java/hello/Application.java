5e624f00a52bddf68abf126dc564c4774eabd96e
K Song <kyungsong42@bitbucket.org>
2017-06-28 10:29:24 +0900

    commit for website

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Kyung Song on 6/28/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/WebController.java
@@ -0,0 +1,21 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+/**
+ * Created by Kyung Song on 6/28/2017.
+ */
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required = false, defaultValue = "World!") String name, Model model)
+    {
+        model.addAttribute("name", name);
+        return "greeting";
+
+    }
+}
